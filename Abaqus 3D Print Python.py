7123744534b5794284441db86b65330ba4725728
CAE ASSISTANT <caeassistantcompany@gmail.com>
2025-03-18 12:11:57 +0330

    Add files via upload

---
+++ Abaqus 3D Print Python.py
@@ -0,0 +1,49 @@
+
+#      ######################################################################
+#      #################      CAE Assistant Company          ################
+#      ##############         www CAEassistant com              #############
+#      ###########   Copy right by CAE Assistant Company    ###############
+##      ######################################################################
+#      ONLY the BUYER  of this package has permission to use its codes.
+#	   Any distribution of this subroutine is illegal and will be prosecuted 
+#      ######################################################################
+#      ######################################################################
+#      CAE Assisitant Services: 
+#      Toturial Packages,Consultancy,Articles,Q&A,Video Gallery,Online Course
+#      ######################################################################
+#      Need help with your project? 
+#      You can get initial free consultation from (Support CAEassistant com)
+#      ######################################################################
+from abaqus import *
+from abaqusConstants import *
+
+# User inputs for geometric parameters, material properties, and other settings
+# *** Some details have been hidden. For the full implementation, visit: ***
+# https://caeassistant.com/product/3d-printing-simulation-in-abaqus/
+
+fields = (('path directory','E:/'))
+Pathd=getInputs(fields=fields,dialogTitle='Specify path directory', )
+
+fields = (('thickness of layer:','[HIDDEN]'), ('number of layers:', '[HIDDEN]'), ('x reference:', '[HIDDEN]'), ('z reference:', '[HIDDEN]'))
+h, n, xc,zc=getInputs(fields=fields,dialogTitle='Specify Geometric Parameters', )
+
+fields = (('k1','[HIDDEN]'), ('theta1k:', '[HIDDEN]'), ('k2:', '[HIDDEN]'), ('theta2k:', '[HIDDEN]'))
+k1,theta1k , k2,theta2k=getInputs(fields=fields,dialogTitle='Conductivity', )
+
+# Material properties
+mdb.models['Model-1'].Material(name='Material-1')
+mdb.models['Model-1'].materials['Material-1'].Conductivity(
+    temperatureDependency=ON, table=((k1, theta1k), (k2, theta2k)))
+mdb.models['Model-1'].materials['Material-1'].Density(table=(('[HIDDEN]', ), ))
+mdb.models['Model-1'].materials['Material-1'].LatentHeat(table=(('[HIDDEN]', '[HIDDEN]', '[HIDDEN]'), ))
+mdb.models['Model-1'].materials['Material-1'].SpecificHeat(
+    temperatureDependency=ON, table=(('[HIDDEN]', '[HIDDEN]'), ('[HIDDEN]', '[HIDDEN]')))
+
+# Assembly process (Hidden)
+# *** Full assembly details are available in the package: ***
+# https://caeassistant.com/product/3d-printing-simulation-in-abaqus/
+
+# Mesh generation (Hidden)
+# Job submission
+mdb.Job(name='Job-1', model='Model-1', userSubroutine=Pathd[0]+'\Subroutine-1.for')
+mdb.jobs['Job-1'].submit(consistencyChecking=OFF)
---
+++ Abaqus 3D Print Subroutine.for
@@ -0,0 +1,101 @@
+C      ######################################################################
+C      #################      CAE Assistant Company          ################
+C      ##############         CAEassistant-com              #############
+C      ###########   Copy right by CAE Assistant Company    ###############
+C      ######################################################################
+
+C      ######################################################################
+C      ######################################################################
+C      CAE Assisitant Services: 
+C      Toturial Packages,Consultancy,Articles,Q&A,Video Gallery,Online Course
+C      ######################################################################
+C      Need help with your project? 
+C      You can get initial free consultation from (our website caeassistant com)
+C      ###################################################################### 	
+C*** This is a user subroutine for Abaqus ***
+C *** Some details have been hidden. For the full implementation, visit: ***
+C *** https://caeassistant.com/product/3d-printing-simulation-in-abaqus/ ***
+      SUBROUTINE USDFLD(FIELD,STATEV,PNEWDT,DIRECT,T,CELENT,
+     1 TIME,DTIME,CMNAME,ORNAME,NFIELD,NSTATV,NOEL,NPT,LAYER,
+     2 KSPT,KSTEP,KINC,NDI,NSHR,COORD,JMAC,JMATYP,MATLAYO,LACCFLA)
+C
+      INCLUDE 'ABA_PARAM.INC'
+C
+      CHARACTER*80 CMNAME,ORNAME
+      CHARACTER*80 CPNAME,partname,str,str2   
+      CHARACTER*3  FLGRAY(15)
+      DIMENSION FIELD(NFIELD),STATEV(NSTATV),DIRECT(3,3),
+     1 T(3,3),TIME(2)
+      DIMENSION ARRAY(15),JARRAY(15),JMAC(*),JMATYP(*),COORD(*)
+	REAL*8 EMOD,ENU,EBULK3,EG2,EG,EG3,ELAM,ONE,TWO,THREE,YE,DSTRESS(6),
+     1etag,etak,alfaa,alfab,alfac,Ec,Ymax,alfa,kp,gama,Dp,n,d,E0,emax,
+     2curtime,E(KSTEP),EY
+      integer i,j      
+      
+	ONE=1.d0
+	TWO=2.d0
+	THREE=3.d0
+
+C=======================INPUTS===================================
+	 Ec=0.8E-3
+	 Ymax=2.88E9
+	 alfa=0.31
+	 Dp=0.88E-3
+C ***[HIDDEN]
+C      print*,'TIME(2)===================================',TIME(2)       
+      
+      DO j=1,KSTEP
+	Evs=0.0
+      DO i=1,J
+      Evs=Evs+exp(-(i-1)*(d/Dp))
+      END DO
+      E(j)=E0*exp(-COORD(3)/Dp)*Evs
+      END DO
+C      print*,'KSTEP=',KSTEP
+C      print*,'E=',E
+      CALL GETPARTINFO(NOEL, 1, CPNAME, LOCNUM, JRCD)      
+      
+C ***[HIDDEN]
+
+       END DO
+       STATEV(2)=(ONE-exp(-alfa*TIME(2)))
+       STATEV(3)=(ONE-exp(-beta*(EY/Ec-ONE)))
+
+       
+C       print*,'COORD(3)=',COORD(3)
+C       print*,'EY=',EY
+C       print*,'Y=',Y
+C       print*,'NPT=',NPT
+C       print*,'NOEL=',NOEL
+C       print*,'CPNAME=',CPNAME
+   
+       FIELD(1)=Y      
+       STATEV(1)=EY
+
+      RETURN  
+      END
+
+      SUBROUTINE  DISP(U,KSTEP,KINC,TIME,NODE,NOEL,JDOF,COORDS)
+C
+      INCLUDE 'ABA_PARAM.INC'
+C
+      DIMENSION U(3),TIME(2),COORDS(3)
+      CHARACTER*80 CPNAME,partname,str
+      REAL*8 d,Tmax,tp,T
+C
+      d=2e-3    !Thickness of layer
+      Tmax=80.d0  !Maximum Temperature
+      tp=1.d0    !Time period for every step
+C ***[HIDDEN]
+      print*,'TIME(2)===================================',TIME(2)      
+C=========Specify layer number=i================================= 
+
+C ***[HIDDEN]
+   !   print*,'x=',x
+      
+C=========Temperature function===================================       
+
+        print*,' U(1)=', U(1)
+
+      RETURN
+      END
